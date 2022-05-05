ecacbd9eb1a37ca89e16baac3c928b8f7a5be216
bom\bomi <kk3p1q1@gmail.com>
2022-05-05 19:16:46 +0900

    초기세팅

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.less";
+import { Layout } from "antd";
+import { Header, Content, Footer } from "antd/lib/layout/layout";
+
+function App() {
+  return (
+    <Layout>
+      <Header>Header</Header>
+      <Content>Content</Content>
+      <Footer>Footer</Footer>
+    </Layout>
+  );
+}
+
+export default App;
